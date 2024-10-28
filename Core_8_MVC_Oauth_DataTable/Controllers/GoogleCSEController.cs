bd56d39f881e23a5d2787d101b9f5c5185274467
David07994415 <david.game.power@gmail.com>
2024-10-28 22:34:25 +0800

    Feat:Add Google CSE Controller

---
+++ Core_8_MVC_Oauth_DataTable/Controllers/GoogleCSEController.cs
@@ -0,0 +1,40 @@
+﻿using Core_8_MVC_Oauth_DataTable.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_8_MVC_Oauth_DataTable.Controllers
+{
+	public class GoogleCSEController : Controller
+	{
+		private readonly IConfiguration _configuration;
+
+		public GoogleCSEController(
+			IConfiguration configuration
+			)
+		{
+
+			_configuration = configuration;
+			
+		}
+
+
+
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult redirect([FromForm] string search)
+		{
+
+
+			string YourGoogleID = _configuration.GetSection("GoogleCseId").Value!; //"YourGoogleID";
+			string googleCsePath = $@"https://www.google.com/cse?cx={YourGoogleID}&ie=UTF-8&q={Uri.EscapeDataString(search)}";
+	
+
+
+			return Redirect(googleCsePath);
+		}
+	}
+}
