891c62d0aef556bce380191bf6839f5ee46b6514
David07994415 <david.game.power@gmail.com>
2024-11-21 22:14:26 +0800

    Feat:Add MultiPolgon Map Controller

---
+++ Core_8_MVC_Oauth_DataTable/Controllers/MapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_8_MVC_Oauth_DataTable.Controllers
+{
+	public class MapController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
