5561b9d153649365f243929960ad8eb96930dce3
David07994415 <david.game.power@gmail.com>
2024-12-04 22:00:28 +0800

    Feat:Add Set Cookie

---
+++ Core_8_MVC_Oauth_DataTable/Controllers/SetCookieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Core_8_MVC_Oauth_DataTable.Controllers
+{
+    public class SetCookieController : Controller
+    {
+        public IActionResult Index()
+        {
+            // 創建一個包含多個項目的字典
+            var userData = new Dictionary<string, string>
+            {
+                { "AreaCode", "02" },
+                { "DivCode", "01" }
+            };
+
+            // 將字典序列化為 JSON 字符串
+            var json = JsonSerializer.Serialize(userData);
+
+            // 設定 Cookie，將 JSON 字符串存儲在 "UserData" Cookie 中
+            Response.Cookies.Append("CookieName", json, new CookieOptions
+            {
+                Expires = DateTime.Now.AddHours(1),
+                HttpOnly = true,
+                Secure = true
+            });
+
+            return View();
+        }
+    }
+}
---
+++ Core_8_MVC_Oauth_DataTable/ViewComponents/CookieItem.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Core_8_MVC_Oauth_DataTable.ViewComponents
+{
+    public class CookieItem: ViewComponent
+    {
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var data = await _apiList.GetApiConnectionData();
+            //// 預設資料夾：/Views/Shared/Components/{檢視元件名稱}/{檢視名稱}
+
+            //if (TestParam != null)
+            //{
+            //    ViewBag.TestPara = TestParam;
+            //}
+
+            // 讀取 Cookie 並反序列化為字典
+            var userDataJson = Request.Cookies["CookieName"];
+            if (userDataJson != null)
+            {
+                var userData = JsonSerializer.Deserialize<Dictionary<string, string>>(userDataJson);
+
+                // 顯示 Cookie 的值
+                ViewBag.CookieContent = $"UserId: {userData["AreaCode"]}, UserName: {userData["DivCode"]}";
+            }
+            else
+            {
+                ViewBag.CookieContent = "No user data found in cookie.";
+            }
+
+
+
+            return View();
+        }
+
+
+    }
+}
